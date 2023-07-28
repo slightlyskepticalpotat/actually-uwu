ed66d51f0c9691479e338b0c07aef10549b2c4f3
siumairice <nicoleam.go@gmail.com>
2023-07-28 09:53:59 -0400

    initial t3 stack setup fix

--- react-frontend/src/pages/_app.tsx
+++ react-frontend/src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "?/styles/globals.css";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
-export default MyApp;
+export default api.withTRPC(MyApp);
