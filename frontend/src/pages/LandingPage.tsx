fef719839a99b2c8a57961048ea14f88fe71bb8c
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2023-07-29 12:21:24 -0400

    Update LandingPage.tsx

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -56,7 +56,6 @@ const useStyles = makeStyles({
 // dark mode fuckery
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
