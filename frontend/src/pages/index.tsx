994385d0d49e4cd2e7e731527937fdfaaa1aa417
siumairice <nicoleam.go@gmail.com>
2023-07-29 05:17:21 -0400

    set up oauth login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,19 @@
 import LandingPage from "./LandingPage";
+import React from 'react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 export default function Home() {
   return (
     <>
-      <LandingPage/>
-    </>
+      <Auth0Provider
+         domain="dev-z470o12bjq8b68ip.us.auth0.com"
+         clientId="bl5dRsNTPOvE9GCb8mI0mFtNMYb6kSRq"
+         authorizationParams={{
+           redirect_uri: "http://localhost:3001/SurveyPage"
+         }}
+      >
+          <LandingPage/>
+        </Auth0Provider>
+      </>
   );
 }
