c29a9a2ac0a36448b66b15e31d4b33826bf79df5
siumairice <nicoleam.go@gmail.com>
2023-07-29 09:58:21 -0400

    fix weather data fetching

--- frontend/src/pages/Weather.tsx
+++ frontend/src/pages/Weather.tsx
@@ -1,24 +1,28 @@
 import { Box } from '@mui/material';
 import React, { useState } from 'react';
+import 'dotenv/config'
 
 const Weather: React.FC = () => {
   const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState<any>(null);
-  const [latitude, setLatitude] = useState(43.653226);
-  const [longitude, setLongitude] = useState(-79.383184);
+  const [weatherData, setWeatherData] = useState(null);
+  const [latitude, setLatitude] = useState(43.65);
+  const [longitude, setLongitude] = useState(-79.38);
 
   const fetchWeatherData = async () => {
     try {
         const options = {method: 'GET', headers: {accept: 'application/json'}};
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY}`, options)
-          .then(response => response.json())
-          .then(response => {setWeatherData(response)
-                            console.log(response)})
-          .catch(err => console.error(err));
+        // console.log(process.env.API_KEY)
+
+        const APIREQ = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon="+ longitude + "&appid=c0f957daa1315f627f7244c78fc760e7"
+        const response = await fetch(APIREQ);
+        const jsonData = await response.json();
+
+        console.log(jsonData)
+        setWeatherData(jsonData)
+
     } catch (error) {
       console.error('Error fetching weather data:', error);
     }
-    
   };
 
   return (
@@ -30,12 +34,15 @@ const Weather: React.FC = () => {
         placeholder="Enter city name"
       />
       <button onClick={fetchWeatherData}>Get Weather</button>
+      <Box>
 
-      {weatherData && (
-        <Box>
-            {weatherData.message}
-        </Box>
-      )}
+        {weatherData? (weatherData["timezone"]) : <p>no data yet</p>}
+        {weatherData ? (
+          <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+        ) : (
+          <p>No data fetched yet.</p>
+        )}
+      </Box>
     </Box>
   );
 };
