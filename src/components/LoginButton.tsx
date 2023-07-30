f05612d9d1a5e12ca3279f47bbb1ba03861c8fd2
Elizabeth <35873939+Lucifersan@users.noreply.github.com>
2023-07-29 18:44:06 -0700

    fix login

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,23 +1,28 @@
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 import React from "react";
+import Link from "next/link";
 
 const useStyles = makeStyles({
-  loginButton:{
-    width: '30%',
-    padding: '1rem 2rem',
-    borderRadius: '1rem',
-    border: '2px solid #546D64',
-    backgroundColor:'transparent',
-    '&:hover': {
-        backgroundColor: '#f0f0f0',
-    }
-  }
-})
+  loginButton: {
+    width: "30%",
+    padding: "1rem 2rem",
+    borderRadius: "1rem",
+    border: "2px solid #546D64",
+    backgroundColor: "transparent",
+    "&:hover": {
+      backgroundColor: "#f0f0f0",
+    },
+  },
+});
 
 const LoginButton = () => {
-  const classes= useStyles();
+  const classes = useStyles();
 
-  return <a className={classes.loginButton} href="/api/auth/login">Login</a>;
+  return (
+    <Link className={classes.loginButton} href="/api/auth/login">
+      Login
+    </Link>
+  );
 };
 
 export default LoginButton;
